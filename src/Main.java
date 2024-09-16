d4ad5875261c1d9d3366ccaff916ec2bd4688817
Mateusz Krupa <mk131459@stud.ur.edu.pl>
2024-09-16 22:29:32 +0200

    Dodanie klasy Main i Product

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Inicjalizacja interfejsu graficznego
+        GUI gui = new GUI();
+        gui.createAndShowGUI();
+    }
+}
